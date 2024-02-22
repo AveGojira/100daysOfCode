8718fd7bc8bd36a884bef19a63960ece198c228c
AveGojira <157556045+AveGojira@users.noreply.github.com>
2024-02-21 21:52:04 -0600

    Update Day11Blackjack.py
    fixed some bugs with the code that made it a sloppy mess.

--- Day11Blackjack.py
+++ Day11Blackjack.py
@@ -8,14 +8,17 @@ def clear():
 
 
 def deal_card():
+    """Deals random cards from the deck. 10, J, Q, K are all represented by 10"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
 
 def calculate_score(cards):
+    """Calculates the Blackjack and score"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
+    # Changes A value depending on the situation
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
@@ -24,22 +27,22 @@ def calculate_score(cards):
 
 def compare(user_score, computer_score):
     if user_score > 21 and computer_score > 21:
-        return "You went over. You lose."
+        return "You went over. You lose 😤"
 
     if user_score == computer_score:
-        return "Draw."
+        return "Draw 🙃"
     elif computer_score == 0:
-        return "Lose, opponent has Blackjack."
+        return "Lose, opponent has Blackjack 😱"
     elif user_score == 0:
-        return "Win with a Blackjack."
+        return "Win with a Blackjack 😎"
     elif user_score > 21:
-        return "You went over. You lose."
+        return "You went over. You lose 😭"
     elif computer_score > 21:
-        return "Opponent went over. You win."
+        return "Opponent went over. You win 😁"
     elif user_score > computer_score:
-        return "You win."
+        return "You win 😃"
     else:
-        return "You lose."
+        return "You lose 😤"
 
 
 def play_game():
@@ -68,13 +71,13 @@ def play_game():
             else:
                 game_over = True
 
-        while computer_score != 0 and computer_score < 17:
-            computer_cards.append(deal_card())
-            computer_score = calculate_score(computer_cards)
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
 
-        print(f"    Your final hand: {user_cards}, final score: {user_score}")
-        print(f"    Computer's final hand: {computer_cards}, final score: {computer_score}")
-        print(compare(user_score, computer_score))
+    print(f"    Your final hand: {user_cards}, final score: {user_score}")
+    print(f"    Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
 
 
 while input("do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
